f83c9ca3b1dffb0ff344d9a142373e33dc43e1f9
Foo-x <www.sand.x@gmail.com>
2020-11-14 19:34:35 +0900

    Update font

--- gatsby-browser.js
+++ gatsby-browser.js
@@ -2,7 +2,7 @@
 import "fontsource-noto-sans-jp"
 import "fontsource-roboto"
 import "fontsource-noto-serif-jp"
-import "fontsource-vollkorn"
+import "fontsource-source-serif-pro"
 // normalize CSS across browsers
 import "./src/normalize.css"
 // custom CSS styles
