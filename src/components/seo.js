30d353340d4c1d11ed3c860a4bc9c71f5a4520a7
Foo-x <www.sand.x@gmail.com>
2020-11-22 00:42:30 +0900

    Update background color

--- src/components/seo.js
+++ src/components/seo.js
@@ -1,16 +1,9 @@
-/**
- * SEO component that queries for data with
- *  Gatsby's useStaticQuery React hook
- *
- * See: https://www.gatsbyjs.com/docs/use-static-query/
- */
-
 import React from "react"
 import PropTypes from "prop-types"
 import { Helmet } from "react-helmet"
 import { useStaticQuery, graphql } from "gatsby"
 
-const SEO = ({ description, lang, meta, title }) => {
+const SEO = ({ description, lang, meta, title, bodyClass }) => {
   const { site } = useStaticQuery(
     graphql`
       query {
@@ -44,6 +37,9 @@ const SEO = ({ description, lang, meta, title }) => {
           href: "/favicon.png",
         },
       ]}
+      bodyAttributes={{
+        class: bodyClass,
+      }}
       meta={[
         {
           name: `description`,
--- src/templates/blog-post.js
+++ src/templates/blog-post.js
@@ -12,6 +12,7 @@ const BlogPostTemplate = ({ data, location }) => {
       <SEO
         title={post.frontmatter.title}
         description={post.frontmatter.description || post.excerpt}
+        bodyClass="body-blog-post"
       />
       <article
         className="blog-post"
