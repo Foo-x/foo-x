a8965afaa09561da79bec80301c67afec930eae2
Foo-x <www.sand.x@gmail.com>
2020-11-28 23:33:36 +0900

    Add 404 page

--- src/pages/404.js
+++ src/pages/404.js
@@ -1,16 +1,51 @@
+import { graphql, Link } from "gatsby"
 import React from "react"
+import Img from "gatsby-image"
 
-import Layout from "../components/layout"
+import LayoutCommon from "../components/layout-common"
 import SEO from "../components/seo"
 
-const NotFoundPage = ({ location }) => {
+const NotFoundPage = ({ data, location }) => {
+  console.log(data)
   return (
-    <Layout location={location}>
-      <SEO title="404: Not Found" location={location} />
-      <h1>404: Not Found</h1>
-      <p>You just hit a route that doesn&#39;t exist... the sadness.</p>
-    </Layout>
+    <LayoutCommon titleSvg={data.notFound.publicURL}>
+      <SEO title="404" location={location} />
+      <article
+        className="not-found-page"
+        itemScope
+        itemType="http://schema.org/Article"
+      >
+        <Img
+          fluid={data.sleepingCat.childImageSharp.fluid}
+          alt="sleeping-cat"
+        />
+        <section itemProp="articleBody">
+          <Link to="/">トップへ</Link>
+        </section>
+      </article>
+    </LayoutCommon>
   )
 }
 
 export default NotFoundPage
+
+export const pageQuery = graphql`
+  query {
+    notFound: file(
+      sourceInstanceName: { eq: "assets" }
+      relativePath: { eq: "404.svg" }
+    ) {
+      publicURL
+    }
+    sleepingCat: file(
+      sourceInstanceName: { eq: "assets" }
+      relativePath: { eq: "sleeping-cat.jpg" }
+    ) {
+      childImageSharp {
+        fluid {
+          ...GatsbyImageSharpFluid_noBase64
+        }
+      }
+    }
+  }
+`
