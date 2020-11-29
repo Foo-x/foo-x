a552743dd369607d486d51d83bee9f936a5173f3
Foo-x <www.sand.x@gmail.com>
2020-11-29 16:39:47 +0900

    Update thumbnail vector class

--- src/components/thumbnail-vector.js
+++ src/components/thumbnail-vector.js
@@ -3,11 +3,11 @@ import React, { useState } from "react"
 const ThumbnailVector = ({ img }) => {
   const [isHidden, setIsHidden] = useState(true)
 
-  const className = "post-list-item-image-vector"
+  const className = "thumbnail-vector"
   const classNames = isHidden ? [className, "is-hidden"] : [className]
 
   return (
-    <div className="post-list-item-image-space">
+    <div className="thumbnail-vector-space">
       <img
         src={img}
         className={classNames.join(" ")}
