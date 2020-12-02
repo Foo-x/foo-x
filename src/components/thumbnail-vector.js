725efcd002de08051bbf0a62e1fed17df4897edf
Foo-x <www.sand.x@gmail.com>
2020-12-02 22:40:50 +0900

    Fix error on cached image

--- src/components/thumbnail-vector.js
+++ src/components/thumbnail-vector.js
@@ -1,7 +1,11 @@
-import React, { useState } from "react"
+import React, { useRef, useState } from "react"
 
 const ThumbnailVector = ({ img }) => {
   const [isHidden, setIsHidden] = useState(true)
+  const target = useRef(null)
+  if (target.current?.complete && isHidden) {
+    setIsHidden(false)
+  }
 
   const className = "thumbnail-vector"
   const classNames = isHidden ? [className, "is-hidden"] : [className]
@@ -9,6 +13,7 @@ const ThumbnailVector = ({ img }) => {
   return (
     <div className="thumbnail-vector-space">
       <img
+        ref={target}
         src={img}
         className={classNames.join(" ")}
         alt="thumbnail"
