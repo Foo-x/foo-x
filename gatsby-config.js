8b04daf8cc9e3dae62a3d6ac1ab33edffc1b2898
Foo-x <www.sand.x@gmail.com>
2020-12-04 21:44:07 +0900

    Update link to be external

--- gatsby-config.js
+++ gatsby-config.js
@@ -47,6 +47,7 @@ module.exports = {
           `gatsby-remark-smartypants`,
           `gatsby-remark-line-breaks`,
           `gatsby-remark-twemoji-shortcut`,
+          `gatsby-remark-external-links`,
         ],
       },
     },
