0e8b2bcddeb9725eea4615bb0207b09e213ae5ad
Foo-x <www.sand.x@gmail.com>
2022-02-05 10:58:03 +0900

    chore: update font preload settings

---
+++ bin/removeBase64.js
@@ -0,0 +1,16 @@
+const { readFileSync, writeFileSync } = require("fs")
+const path = require("path")
+
+const cachePath = path.resolve(__dirname, "../font-preload-cache.json")
+
+const cache = JSON.parse(readFileSync(cachePath))
+
+for (const fontMap of Object.values(cache.assets)) {
+  Object.keys(fontMap)
+    .filter(font => font.startsWith("data:"))
+    .forEach(font => {
+      delete fontMap[font]
+    })
+}
+
+writeFileSync(cachePath, JSON.stringify(cache))
