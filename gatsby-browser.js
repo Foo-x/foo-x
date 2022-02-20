a4a3056d69497e2166dcb7e4d3522e78dcde8cd2
Foo-x <www.sand.x@gmail.com>
2022-02-20 23:27:23 +0900

    style: use bold fonts

--- gatsby-browser.js
+++ gatsby-browser.js
@@ -1,6 +1,8 @@
 // custom typefaces
 import "@fontsource/noto-sans-jp"
 import "@fontsource/roboto"
+import "@fontsource/noto-sans-jp/700.css"
+import "@fontsource/roboto/700.css"
 // normalize CSS across browsers
 import "./src/normalize.css"
 // custom CSS styles
